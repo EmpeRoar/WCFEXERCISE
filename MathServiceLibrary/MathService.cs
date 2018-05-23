cf91fc1c55d99fbf466f38d1fa4641f54ce99d84
Julius Bacosa <juliusbacosa@gmail.com>
2018-05-23 21:30:56 +0800

    wcf library

---
+++ MathServiceLibrary/MathService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MathServiceLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class MathService : IMathService
+    {
+        public int Add(MyNumbers obj)
+        {
+            return (obj.Number1 + obj.Number2);
+        }
+
+        public int Substract(MyNumbers obj)
+        {
+            return (obj.Number1 - obj.Number2);
+        }
+
+        public int Mutiply(MyNumbers obj)
+        {
+            return (obj.Number1 * obj.Number2);
+        }
+    }
+}
